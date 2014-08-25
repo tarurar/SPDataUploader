f2bdd03a86598c27101a160f30e9d3ac92925b1b
tarurar <tarutin_andrew@mail.ru>
2014-08-25 18:36:46 +0400

    new Upload method

--- SPDataUpload/ListDataUploader.cs
+++ SPDataUpload/ListDataUploader.cs
@@ -13,6 +13,23 @@ namespace SPDataUpload
 {
     public class ListDataUploader : Uploader
     {
+        public void UploadToListByFileName(string csvPath, string csvDelimiter, ClientContext ctx)
+        {
+            if (!File.Exists(csvPath))
+                throw new FileNotFoundException();
+
+            string fn = Path.GetFileNameWithoutExtension(csvPath);
+
+            IEnumerable<List> allLists = ctx.LoadQuery(ctx.Web.Lists.Include(inc => inc.RootFolder, inc => inc.RootFolder.Name));
+            ctx.ExecuteQuery();
+
+            List targetList = allLists.SingleOrDefault(l => l.RootFolder.Name == fn);
+
+            if (targetList == null)
+                throw new FileNotFoundException(String.Format("List {0} not found", fn));
+
+            Upload(csvPath, csvDelimiter, targetList, ctx);
+        }
         public void Upload(string csvPath, string csvDelimiter, List targetList, ClientContext ctx)
         {
             if (!File.Exists(csvPath))
