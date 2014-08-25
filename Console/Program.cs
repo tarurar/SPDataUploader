deea69e76914889ff521712603cb2b0ecbd6c1c2
tarurar <tarutin_andrew@mail.ru>
2014-08-25 17:52:09 +0400

    added test console project

---
+++ Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using SPDataUpload;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ClientContext ctx = new ClientContext(""))
+            {
+                ctx.ExecutingWebRequest += (sender, eventArgs) =>
+                {
+                    eventArgs.WebRequestExecutor.RequestHeaders.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f");
+                };
+                ctx.Credentials = new NetworkCredential("", "", "");
+
+                IEnumerable<List> allLists = ctx.LoadQuery(ctx.Web.Lists.Include(inc => inc.RootFolder, inc => inc.RootFolder.Name));
+                ctx.ExecuteQuery();
+                
+                List targetList = allLists.SingleOrDefault(l => l.RootFolder.Name == "testImportList");
+                if (targetList != null)
+                {
+                    ListDataUploader uploader = new ListDataUploader();    
+                    uploader.Upload("TestList3.txt", ",", targetList, ctx);
+                }
+            }
+        }
+    }
+}
