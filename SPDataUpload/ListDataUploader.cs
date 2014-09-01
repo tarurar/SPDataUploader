4cf0a22e4edaf870e4b847999e44dea80dbc1668
tarurar <tarutin_andrew@mail.ru>
2014-09-01 18:16:21 +0400

    new option: delete existant data

--- SPDataUpload/ListDataUploader.cs
+++ SPDataUpload/ListDataUploader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint.Client;
@@ -13,20 +14,39 @@ namespace SPDataUpload
 {
     public class ListDataUploader : Uploader
     {
-        public void UploadToListByFileName(string csvPath, string csvDelimiter, ClientContext ctx)
+        public void UploadToListByFileName(string csvPath, string csvDelimiter, ClientContext ctx, bool deleteData = false)
         {
             if (!File.Exists(csvPath))
                 throw new FileNotFoundException();
 
             string fn = Path.GetFileNameWithoutExtension(csvPath);
 
-            IEnumerable<List> allLists = ctx.LoadQuery(ctx.Web.Lists.Include(inc => inc.RootFolder, inc => inc.RootFolder.Name));
+            IEnumerable<List> allLists = ctx.LoadQuery(ctx.Web.Lists.Include(inc => inc.RootFolder, inc => inc.RootFolder.Name, inc => inc.ItemCount));
             ctx.ExecuteQuery();
 
             List targetList = allLists.SingleOrDefault(l => l.RootFolder.Name.Equals(fn, StringComparison.CurrentCultureIgnoreCase));
 
             if (targetList == null)
                 throw new FileNotFoundException(String.Format("List {0} not found", fn));
+
+            if (targetList.ItemCount > 0)
+            {
+                if (deleteData)
+                {
+                    ListItemCollection items = targetList.GetItems(CamlQuery.CreateAllItemsQuery());
+                    ctx.Load(items);
+                    ctx.ExecuteQuery();
+
+                    for (int i = targetList.ItemCount - 1; i > -1; i--)
+                    {
+                        items[i].DeleteObject();
+                        ctx.ExecuteQuery();
+                    }
+                }
+                else
+                    throw new Exception(
+                        String.Format("List {0} has items. Specify clearly that the existant data has to be deleted.", fn));
+            }
 
             Upload(csvPath, csvDelimiter, targetList, ctx);
         }
